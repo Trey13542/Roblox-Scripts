62ea7e832013e87eccd0908d0863115a4b599417
Trey13542 <treyjhancock@hotmail.com>
2024-08-07 01:47:10 -0400

    Update config.lua

--- Utilities/config.lua
+++ Utilities/config.lua
@@ -9,11 +9,11 @@ assert(writefile and readfile, 'exploit not supported')
 
 
 --< [functions] >--
-
 function saveConfig(name, config)
 	writefile(name, game:GetService('HttpService'):JSONEncode(config))
 end
 function loadConfig(name, config)
-	if not pcall(function() readfile(name) end) then writefile(name, game:GetService('HttpService'):JSONDecode(config)) end
-	return game:GetService('HttpService'):JSONDecode(readfile(name))
+	local content = ''
+	pcall(function() content = game:GetService('HttpService'):JSONDecode(readfile(name)) end)
+	return content or ''
 end
